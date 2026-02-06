0043cc024c52a1fcea5b17b4b8a8d33f42c3a23f
salahram390-sudo <salahram390@gmail.com>
2026-02-06 13:42:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
